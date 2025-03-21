d7ffb49fc7bafdcc26e1dce860e7ac88dcda657e
kaustubh-ronge <166821543+kaustubh-ronge@users.noreply.github.com>
2025-03-21 16:30:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+    function checkPassword() {
+      const passwordInput = document.getElementById('passwordInput').value;
+      const errorMessage = document.getElementById('errorMessage');
+      const successMessage = document.getElementById('successMessage');
 
+      // Set the correct password here
+      const correctPassword = "Mountain12345";
+
+      if (passwordInput === correctPassword) {
+        successMessage.classList.remove('hidden');
+        errorMessage.classList.add('hidden');
+      } else {
+        errorMessage.classList.remove('hidden');
+        successMessage.classList.add('hidden');
+      }
+    }
+  
